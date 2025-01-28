2736cf508712825ec3e2e99b55c04955e783a4bb
Leo D'Arcy <leo.darcy@outlook.com>
2025-01-28 00:02:43 +0000

    Fix issue where null values where being introduced into the registry when migrating from old GPOs. Include script to remove null values from GPOs which were previously migrated

---
+++ DPCManagement/Scripts/Clear-NullFromRegistry.ps1
@@ -0,0 +1,37 @@
+﻿$RegList = @("HKLM:\SOFTWARE\Policies\DPC\DPCClient","HKLM:\SOFTWARE\DPC\DPCClient")
+
+Function Clear-NullChars
+{
+    param(
+        [string]
+        $RegKey
+    )
+    Write-Output "Checking $RegKey"
+    $RegKey = $RegKey.Replace("HKEY_LOCAL_MACHINE","HKLM:")
+
+    $Item = Get-Item -Path $RegKey
+
+    foreach ($Value in $Item.Property)
+    {
+        Write-Output "Checking $Value"
+        $Data = Get-ItemPropertyValue -Path $RegKey -Name $Value
+        if ($Data.GetType() -eq [int]) {continue}
+        $SanitisedData = $Data.Replace("`0","")
+        if ($Data.Length -ne $SanitisedData.Length) #Null is not compared but does change the length of the string
+        {
+            Write-Warning "Null Found, Fixing..."
+            Set-ItemProperty -Path $RegKey -Name $Value -Value $SanitisedData
+        }
+    }
+
+    $SubKeys = Get-ChildItem -Path $RegKey
+    foreach ($Key in $SubKeys)
+    {
+        Clear-NullChars -RegKey $Key.Name
+    }
+}
+
+foreach ($RegKey in $RegList)
+{
+    Clear-NullChars -RegKey $RegKey
+}
