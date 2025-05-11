477b5d05e50c940ffc54baeace8656334792b925
Leo D'Arcy <leo.darcy@outlook.com>
2025-05-11 11:50:02 +0100

    Add simple interactive test endpoint for debugging. This should never ship in the released build

---
+++ DPCDevClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using DPCLibrary.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPCDevClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string profileName = "AOVPN User Tunnel";
+            var profiles = ManageRasphonePBK.ListHiddenProfiles(profileName);
+            foreach (var profile in profiles)
+            {
+                Console.WriteLine($"Profile: {profile}");
+            }
+        }
+    }
+}
