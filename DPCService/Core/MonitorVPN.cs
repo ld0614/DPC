2b8396ab284c656664a74b773fae95d8ea61213f
Leo D'Arcy <leo.darcy@outlook.com>
2026-04-10 20:27:22 +0100

    Improve code quality

--- DPCService/Core/MonitorVPN.cs
+++ DPCService/Core/MonitorVPN.cs
@@ -271,8 +271,8 @@ namespace DPCService.Core
             {
                 IList<NetworkInterface> adapters = AccessNetInterface.GetLocalNetworkInterfaces();
 
-                usingIPv4 = adapters.Where(n => AccessNetInterface.InterfaceHasIPv4Gateway(n)).Count() > 0;
-                usingIPv6 = adapters.Where(n => AccessNetInterface.InterfaceHasIPv6Gateway(n)).Count() > 0;
+                usingIPv4 = adapters.Count(n => AccessNetInterface.InterfaceHasIPv4Gateway(n)) > 0;
+                usingIPv6 = adapters.Count(n => AccessNetInterface.InterfaceHasIPv6Gateway(n)) > 0;
             }
             catch (Exception ex)
             {
