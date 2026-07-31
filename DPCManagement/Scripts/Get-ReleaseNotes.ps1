51b0a2aa029789f5b87e7b07e6a30f6d378a43fc
Leo D'Arcy <leo.darcy@outlook.com>
2026-07-31 17:40:25 +0100

    Release updated pipelines with automated release capabilities

---
+++ DPCManagement/Scripts/Get-ReleaseNotes.ps1
@@ -0,0 +1,64 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("Full Release", "Release Candidate", "Preview", "Beta")]
+    [string]
+    $ReleaseType,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Version,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $InstallerVersion
+)
+
+$README = Get-Content .\README.md | Out-String
+
+$Sections = $README.Split("## ")
+
+$ReleaseNotes = ""
+$vNextReleaseNotes = ""
+
+foreach ($Section in $Sections)
+{
+    if ($Section.Trim().StartsWith("Version $Version"))
+    {
+        $ReleaseNotes = $Section.Trim().Replace("Version $Version","").Trim()
+        break
+    }
+
+    if ($Section.Trim().StartsWith("Version vNext"))
+    {
+        $vNextReleaseNotes = $Section.Trim().Replace("Version vNext","").Trim()
+    }
+}
+
+if ([string]::IsNullOrWhiteSpace($ReleaseNotes) -and -NOT [string]::IsNullOrWhiteSpace($vNextReleaseNotes) -and $ReleaseType -ne "Full Release")
+{
+    $ReleaseNotes = $vNextReleaseNotes
+}
+
+if ([string]::IsNullOrWhiteSpace($ReleaseNotes))
+{
+    throw "Unable to locate release notes for $Version"
+}
+
+$ReleaseText = "Release Notes:`n`n$ReleaseNotes`n`n"
+
+if ($InstallerVersion -ne $Version)
+{
+    $ReleaseText += "Please note that to enable automatic upgrades to the released version, this release has a internal build version of $InstallerVersion`n`n"
+}
+
+$ReleaseText += @"
+Check out https://aovpndpc.com/#community for access to the community
+
+Contact DPC@darcy.org.uk if you're interested in Commercial Support
+"@
+
+#Export values back to the pipeline
+Add-Content -Path $env:GITHUB_OUTPUT -Value "releaseNotes=$ReleaseNotes"
+
+$delimiter = "EOF_$([guid]::NewGuid().ToString())" #Use a unique value to avoid EOF being picked up from the variable itself
+Add-Content -Path $env:GITHUB_OUTPUT -Value "releaseText<<$delimiter"
+Add-Content -Path $env:GITHUB_OUTPUT -Value $ReleaseText
+Add-Content -Path $env:GITHUB_OUTPUT -Value $delimiter
